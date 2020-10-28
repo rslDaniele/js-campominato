12393de33bf6857f91e4bf2fd4657a679ba43587
Daniele <danlup94@gmail.com>
2020-10-28 23:28:55 +0100

    Inserimento codice js
    regole del gioco con un array da 16 numeri, ma una sola scelta dell'utente.

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -6,3 +6,44 @@
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+
+// Dichiarazione funzione che genera i 16 numeri
+
+function generaNum() {
+  for (var i = 0; i < 16; i++) {
+  var numeroGen = Math.floor(Math.random() * 100);
+  console.log("numeri estratti", numeroGen )
+  numeri.push(numeroGen); //funzione che mette i numeri generati nell'array
+  }
+}
+
+
+var numeri = []; // Variabile d'appoggio da usare come array per i numeri generati
+
+generaNum(); // Comando per eseguire la funzione
+console.log(numeri);
+
+// Chidiamo all'utente di inserire 16 numeri tra 1 e 100
+
+var sceltaUser = prompt("Scegli un numero da 1 a 16");
+console.log(sceltaUser);
+
+// Controlliamo se il numero scelto è nell'array
+
+var win = 1;
+
+for (var i = 0; i < numeri.length; i++) {
+  var numeroCheck = numeri[i];
+  console.log("esecuzione ciclo per", numeroCheck)
+
+  if (numeroCheck == sceltaUser) {
+    win = 0;
+  }
+}
+
+if (win == 0) {
+  console.log("hai perso!")
+} else {
+  console.log("andiamo avanti!")
+}
